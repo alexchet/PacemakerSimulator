8f2fd1c0b1bce45c0acdc8976ff8bc98fc137238
Stella <stella.engle@hotmail.com>
2017-03-13 13:07:54 +0000

    Fixed the combo boxes

--- src/DCMSimulation.java
+++ src/DCMSimulation.java
@@ -25,11 +25,18 @@ import javax.swing.DefaultComboBoxModel;
 import javax.swing.SwingConstants;
 import java.awt.Component;
 import java.awt.FlowLayout;
+import javax.swing.JTextField;
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
 
 public class DCMSimulation extends JFrame{
 	JPanel contentPane, initialPanel, secondPanel;
 	Panel heartBeatPanel, normalPanel;
 	ArrayList<Point> pacedList = new ArrayList<Point>();
+	private JTextField textField;
+	PacingModes pm;
+	SensingModes sm;
+	ResponseModes rm;
 
     /**
 	 * Create the frame.
@@ -146,12 +153,55 @@ public class DCMSimulation extends JFrame{
 		
 		JComboBox comboBox = new JComboBox();
 		comboBox.setModel(new DefaultComboBoxModel(new String[] {"O - None", "A - Atrium", "V - Ventricle", "D - Dual"}));
+		comboBox.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent arg0) {
+				Object paced = arg0;			
+				if(comboBox.getSelectedIndex()==0) {
+					pm = pm.NONE;
+				}else if(comboBox.getSelectedIndex()==1) {
+					pm= pm.ATRIUM;
+				}else if(comboBox.getSelectedIndex()==2) {
+					pm = pm.VENTRICAL;
+				}else if(comboBox.getSelectedIndex()==3) {
+					pm = pm.DUAL;
+				}
+				
+			}
+		});
+		
 		
 		JComboBox comboBox_1 = new JComboBox();
 		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"O - None", "A - Atrium", "V - Ventricle", "D - Dual"}));
-		
+		comboBox_1.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent arg0) {
+		if(comboBox_1.getSelectedIndex()==0) {
+			sm = sm.NONE;
+		}else if(comboBox_1.getSelectedIndex()==1) {
+			sm= sm.ATRIUM;
+		}else if(comboBox_1.getSelectedIndex()==2) {
+			sm = sm.VENTRICAL;
+		}else if(comboBox_1.getSelectedIndex()==3) {
+			sm = sm.DUAL;
+		}
+	}
+		});
+			
 		JComboBox comboBox_2 = new JComboBox();
-		comboBox_2.setModel(new DefaultComboBoxModel(new String[] {"O - None", "A - Atrium", "V - Ventricle", "D -Dual"}));
+		comboBox_2.setModel(new DefaultComboBoxModel(new String[] {"O - None", "T - Triggered", "I - Inhibited", "D - Tracked"}));
+		comboBox_2.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent arg0) {
+		if(comboBox_2.getSelectedIndex()==0) {
+			rm = rm.NONE;
+		}else if(comboBox_2.getSelectedIndex()==1) {
+			rm= rm.TRIGGERED;
+		}else if(comboBox_2.getSelectedIndex()==2) {
+			System.out.println("test");
+			rm = rm.INHIBITED;
+		}else if(comboBox_2.getSelectedIndex()==3) {
+			rm = rm.TRACKED;
+		}
+			}
+		});
 		
 		JLabel lblChambersPaced = new JLabel("Chambers Paced");
 		lblChambersPaced.setFont(new Font("Tahoma", Font.BOLD, 14));
@@ -160,7 +210,7 @@ public class DCMSimulation extends JFrame{
 		lblChambersSensed.setFont(new Font("Tahoma", Font.BOLD, 14));
 		
 		JLabel lblResponseToSensing = new JLabel("Response to Sensing");
-		lblResponseToSensing.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblResponseToSensing.setFont(new Font("Tahoma", Font.BOLD, 14));
 		
 		JLabel label = new JLabel("Patient's Heart Rate");
 		label.setFont(new Font("Tahoma", Font.BOLD, 17));
@@ -183,6 +233,13 @@ public class DCMSimulation extends JFrame{
 		JLabel label_1 = new JLabel("Paced Heart Rate");
 		label_1.setFont(new Font("Tahoma", Font.BOLD, 17));
 		
+		JLabel lblSensedDelay = new JLabel("Sensed Delay");
+		lblSensedDelay.setFont(new Font("Tahoma", Font.BOLD, 16));
+		
+		textField = new JTextField();
+		textField.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		textField.setColumns(10);
+		
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
@@ -190,47 +247,61 @@ public class DCMSimulation extends JFrame{
 					.addContainerGap()
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(btnHeartRate, GroupLayout.PREFERRED_SIZE, 179, GroupLayout.PREFERRED_SIZE)
-							.addGap(28)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addComponent(rdbtnTemporaryState)
-								.addComponent(rdbtnPacenowStae, GroupLayout.PREFERRED_SIZE, 148, GroupLayout.PREFERRED_SIZE)
-								.addComponent(rdbtnPoweronresetState)
-								.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
-									.addComponent(rdbtnPermanentState, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-									.addComponent(lblNewLabel, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-							.addGap(38)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addGroup(gl_contentPane.createSequentialGroup()
-									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-										.addGroup(gl_contentPane.createSequentialGroup()
-											.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-												.addComponent(comboBox, 0, 115, Short.MAX_VALUE)
-												.addComponent(lblChambersPaced, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-											.addGap(7)
-											.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-												.addGroup(gl_contentPane.createSequentialGroup()
-													.addComponent(comboBox_1, GroupLayout.PREFERRED_SIZE, 125, GroupLayout.PREFERRED_SIZE)
-													.addPreferredGap(ComponentPlacement.UNRELATED)
-													.addComponent(comboBox_2, 0, 134, Short.MAX_VALUE))
-												.addGroup(gl_contentPane.createSequentialGroup()
-													.addComponent(lblChambersSensed, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-													.addPreferredGap(ComponentPlacement.UNRELATED)
-													.addComponent(lblResponseToSensing)))
-											.addGap(20))
-										.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
-											.addComponent(rdbtnFixed, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-											.addComponent(lblRecordingModes, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-									.addGap(65))
-								.addGroup(gl_contentPane.createSequentialGroup()
-									.addComponent(rdbtnContinuous, GroupLayout.PREFERRED_SIZE, 139, GroupLayout.PREFERRED_SIZE)
-									.addContainerGap())))
-						.addGroup(gl_contentPane.createSequentialGroup()
 							.addComponent(label, GroupLayout.PREFERRED_SIZE, 171, GroupLayout.PREFERRED_SIZE)
 							.addContainerGap(649, Short.MAX_VALUE))
 						.addGroup(gl_contentPane.createSequentialGroup()
 							.addComponent(label_1)
-							.addContainerGap(764, Short.MAX_VALUE))))
+							.addContainerGap(673, Short.MAX_VALUE))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(btnHeartRate, GroupLayout.PREFERRED_SIZE, 179, GroupLayout.PREFERRED_SIZE)
+							.addGap(12)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(lblNewLabel, GroupLayout.DEFAULT_SIZE, 140, Short.MAX_VALUE)
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(lblChambersPaced, GroupLayout.DEFAULT_SIZE, 120, Short.MAX_VALUE)
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(lblChambersSensed, GroupLayout.DEFAULT_SIZE, 127, Short.MAX_VALUE))
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+										.addComponent(rdbtnPacenowStae, GroupLayout.PREFERRED_SIZE, 148, GroupLayout.PREFERRED_SIZE)
+										.addComponent(rdbtnPoweronresetState)
+										.addGroup(gl_contentPane.createSequentialGroup()
+											.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+												.addGroup(gl_contentPane.createSequentialGroup()
+													.addComponent(rdbtnTemporaryState)
+													.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+													.addComponent(lblSensedDelay))
+												.addGroup(gl_contentPane.createSequentialGroup()
+													.addComponent(rdbtnPermanentState)
+													.addGap(18)
+													.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, 109, GroupLayout.PREFERRED_SIZE)))
+											.addPreferredGap(ComponentPlacement.RELATED)
+											.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+												.addComponent(textField, 0, 0, Short.MAX_VALUE)
+												.addComponent(comboBox_1, 0, 128, Short.MAX_VALUE))))
+									.addGap(8)))
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+									.addGroup(gl_contentPane.createSequentialGroup()
+										.addComponent(rdbtnContinuous, GroupLayout.PREFERRED_SIZE, 139, GroupLayout.PREFERRED_SIZE)
+										.addContainerGap())
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+										.addGroup(gl_contentPane.createSequentialGroup()
+											.addComponent(rdbtnFixed)
+											.addContainerGap())
+										.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+											.addGroup(gl_contentPane.createSequentialGroup()
+												.addComponent(lblResponseToSensing, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+												.addGap(111))
+											.addGroup(gl_contentPane.createSequentialGroup()
+												.addComponent(lblRecordingModes, GroupLayout.DEFAULT_SIZE, 243, Short.MAX_VALUE)
+												.addContainerGap()))))
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addComponent(comboBox_2, GroupLayout.PREFERRED_SIZE, 144, GroupLayout.PREFERRED_SIZE)
+									.addContainerGap())))))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.TRAILING)
@@ -239,37 +310,40 @@ public class DCMSimulation extends JFrame{
 					.addComponent(label, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)
 					.addGap(166)
 					.addComponent(label_1)
-					.addPreferredGap(ComponentPlacement.RELATED, 318, Short.MAX_VALUE)
+					.addGap(196)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(btnHeartRate, GroupLayout.PREFERRED_SIZE, 44, GroupLayout.PREFERRED_SIZE)
 						.addGroup(gl_contentPane.createSequentialGroup()
 							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
 								.addComponent(lblNewLabel)
 								.addComponent(lblChambersPaced)
-								.addComponent(lblResponseToSensing)
-								.addComponent(lblChambersSensed, GroupLayout.PREFERRED_SIZE, 17, GroupLayout.PREFERRED_SIZE))
+								.addComponent(lblChambersSensed, GroupLayout.PREFERRED_SIZE, 17, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblResponseToSensing))
 							.addPreferredGap(ComponentPlacement.RELATED)
 							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-								.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 								.addComponent(comboBox_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-								.addComponent(comboBox_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-								.addComponent(rdbtnPermanentState))))
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(rdbtnPermanentState, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addComponent(comboBox_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+								.addComponent(rdbtnTemporaryState)
+								.addComponent(lblSensedDelay)
+								.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblRecordingModes))
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addComponent(rdbtnPacenowStae)
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(rdbtnPoweronresetState))
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addComponent(rdbtnFixed)
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(rdbtnContinuous)))
+							.addGap(143))
 						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(2)
-							.addComponent(rdbtnTemporaryState)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(rdbtnPacenowStae)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(rdbtnPoweronresetState))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(18)
-							.addComponent(lblRecordingModes)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(rdbtnFixed)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(rdbtnContinuous)))
-					.addGap(14))
+							.addComponent(btnHeartRate, GroupLayout.PREFERRED_SIZE, 44, GroupLayout.PREFERRED_SIZE)
+							.addContainerGap())))
 		);
 
 		new Thread(new Runnable() {
@@ -291,9 +365,12 @@ public class DCMSimulation extends JFrame{
 		     public void run() {
 	    		int countHeartBeats = 0;
 	    		int countPacedBeats = 0;
+	    		if (normalPanel != null) {}
 		        try {
 					while (true) {
 						if (normalPanel != null) { 
+			    			System.out.println(rm);
+			    			normalPanel.setModes(pm, sm, rm);
 							if(heartRate.ratesList.size() > countPacedBeats + 2) {
 								pacedList.add(heartRate.ratesList.get(countPacedBeats));
 								countPacedBeats++;
@@ -301,6 +378,7 @@ public class DCMSimulation extends JFrame{
 								countPacedBeats++;
 							}
 							boolean skipPoint = normalPanel.continousDraw(countHeartBeats, true, true, pacedList);
+					
 							if (skipPoint) {
 								countPacedBeats = countPacedBeats - 2;
 								countHeartBeats--;
